d776c4bc447c97434b750a2eaf7773f71b25defe
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-04-25 02:00:51 -0300

    Add report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj) "
+		 + "  FROM Sale obj"
+		 + " WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))"
+	     + "   AND obj.date BETWEEN :checkedMinDate AND :checkedMaxDate")
+	Optional<SaleMinDTO> searchByParams(LocalDate checkedMinDate, LocalDate checkedMaxDate, String name, Pageable pageable);
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -20,4 +25,28 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SaleMinDTO> findAll(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate checkedMaxDate = checkDate(maxDate, 
+				LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()));
+		
+		LocalDate checkedMinDate = checkDate(minDate, checkedMaxDate.minusYears(1L));
+		
+		Optional<SaleMinDTO> result = repository.searchByParams(checkedMinDate, checkedMaxDate, name, pageable);
+		return null;
+	}
+
+	private LocalDate checkDate(String date, LocalDate localDateDefault) {
+		LocalDate setDate;
+		if (date != null) {
+			setDate = LocalDate.parse(date);
+		}else {
+			setDate = localDateDefault;
+		}
+		
+		return setDate;
+	}
+	
+	
+	
 }
